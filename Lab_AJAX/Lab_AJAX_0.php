981ecee131a6a03be43c24e2f10c651d84f77a2d
Brave <brave_chen@mail.chungyo.net>
2020-08-14 10:59:58 +0800

    modify for class notes and practice

--- Lab_AJAX/Lab_AJAX_0.php
+++ Lab_AJAX/Lab_AJAX_0.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<script src="jquery.min.js"></script>
 </head>
 <body>
 
@@ -14,20 +15,78 @@
 左邊的下拉式選單若改選  C 這項時，右邉的下拉式選單要改成 C01, C02, C03, C04, C05
 
 -->
-
 	<form method="post" action="http://exec.hostzi.com/echo.php">
 		<select name="letter" id="letter">
 			<option value="0">A</option>
 			<option value="1">B</option>
 			<option value="2">C</option>
-		</select>&nbsp;|&nbsp; 
+		</select>
+		&nbsp;|&nbsp;
 		<select name="letterNumber" id="letterNumber">
 			<option value="0">A01</option>
 			<option value="1">B02</option>
 			<option value="2">C03</option>
 		</select> 
-		<input type="submit" value="OK" /> 
+		<input id="aBtn" type="button" value="OK"/> 
 	</form>
+
+	<div id="debug"></div>
+	<script>
+		function setLetterNumber() {
+			var selectedLetter = $("#letter option:selected");
+			var serverUrl = `./getLetterNumber.php?letter=${selectedLetter.text()}`;
+			$.ajax({
+				type: "get",
+				url: serverUrl,
+				// success: function(e) {
+				// 	$("#debug").text(e);
+				// }
+			}).then(function(e) {
+				$("#letterNumber").html(e);
+				// $("#debug").text(e + " v2");
+			});
+		}
+
+		$(document).ready(function() {
+			setLetterNumber();
+		});
+		// $("#letter").trigger("change"); // 也可以debug
+		$("#letter").change(setLetterNumber);
+
+		// $("#letter").change(function() {
+		// 	selectedLetter = $("#letter option:selected");
+		// 	var serverUrl = `./getLetterNumber.php?letter=${selectedLetter.text()}`;
+		// 	$.ajax({
+		// 		type: "get",
+		// 		url: serverUrl,
+		// 		// success: function(e) {
+		// 		// 	$("#debug").text(e);
+		// 		// }
+		// 	}).then(function(e) {
+		// 		$("#letterNumber").html(e);
+		// 		// $("#debug").text(e + " v2");
+		// 	});
+		// 	// $("#debug").text(url);
+		// })
+
+		// $("#aBtn").click( function() {
+		// 	var selectedLetter = $("#letter option:selected");
+		// 	var selectedLetterNumber = $("#letterNumber option:selected");
+		// 	$("#debug").text(selectedLetter.text() + " " + selectedLetterNumber.text());
+		// } )
+
+		// $("#letter").on("change", function() {
+		// 	var selectedLetter = $("#letter option:selected").text();
+		// 	var serverUrl = `./getLetterNumber.php?letter=${selectedLetter}`
+		// 	$.ajax({
+		// 		type: "get",
+		// 		url: serverUrl
+		// 	}).then(function(e) {
+		// 		$("#letterNumber").html(e);
+		// 	})
+		// })
+
+	</script>
 
 </body>
 </html>
